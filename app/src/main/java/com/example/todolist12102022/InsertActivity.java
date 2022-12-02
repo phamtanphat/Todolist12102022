806ee93683f0f01b10d3403f61aaf66aa83a69b8
Phat <phatdroid94@gmail.com>
2022-12-02 18:24:54 +0700

    serializable

--- app/src/main/java/com/example/todolist12102022/InsertActivity.java
+++ app/src/main/java/com/example/todolist12102022/InsertActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InsertActivity extends AppCompatActivity {
 
     @Override
@@ -14,11 +17,18 @@ public class InsertActivity extends AppCompatActivity {
         setContentView(R.layout.activity_insert);
 
         // 1: String
+//        Intent intent = getIntent();
+//        if (intent != null) {
+//            String title = intent.getStringExtra("title");
+//            String message = intent.getStringExtra("message");
+//            Log.d("BBB", title + "\n" + message);
+//        }
+
+        // 2: ArrayList
         Intent intent = getIntent();
         if (intent != null) {
-            String title = intent.getStringExtra("title");
-            String message = intent.getStringExtra("message");
-            Log.d("BBB", title + "\n" + message);
+            ArrayList<String> arrStrings = (ArrayList<String>) intent.getSerializableExtra("arrayList");
+            Log.d("BBB", arrStrings.size()  + "");
         }
     }
 }
--- app/src/main/java/com/example/todolist12102022/MainActivity.java
+++ app/src/main/java/com/example/todolist12102022/MainActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnIntentString;
+    Button btnIntentString, btnIntentArrayListString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnIntentString = findViewById(R.id.button_intent_string);
+        btnIntentArrayListString = findViewById(R.id.button_intent_array_list_string);
 
         btnIntentString.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, InsertActivity.class);
                 intent.putExtra("title", "Thông báo");
                 intent.putExtra("message", "Có dữ liệu mới");
+                startActivity(intent);
+            }
+        });
+
+        btnIntentArrayListString.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<String> arrList = new ArrayList<>();
+                arrList.add("A");
+                arrList.add("B");
+
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                intent.putExtra("arrayList", (Serializable) arrList);
                 startActivity(intent);
             }
         });
