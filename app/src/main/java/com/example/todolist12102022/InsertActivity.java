bee1a5267a7aa753a84a7b4cf02522ce2b8710f5
Phat <phatdroid94@gmail.com>
2022-12-02 18:08:52 +0700

    send string

--- app/src/main/java/com/example/todolist12102022/InsertActivity.java
+++ app/src/main/java/com/example/todolist12102022/InsertActivity.java
@@ -2,7 +2,9 @@ package com.example.todolist12102022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class InsertActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class InsertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        // 1: String
+        Intent intent = getIntent();
+        if (intent != null) {
+            String title = intent.getStringExtra("title");
+            String message = intent.getStringExtra("message");
+            Log.d("BBB", title + "\n" + message);
+        }
     }
 }
--- app/src/main/java/com/example/todolist12102022/MainActivity.java
+++ app/src/main/java/com/example/todolist12102022/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.todolist12102022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
         btnIntentString.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                intent.putExtra("title", "Thông báo");
+                intent.putExtra("message", "Có dữ liệu mới");
+                startActivity(intent);
             }
         });
     }
