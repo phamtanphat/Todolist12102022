41c8f36da5a21f9222d99ce61a4166438839c60f
Phat <phatdroid94@gmail.com>
2022-12-05 19:00:59 +0700

    ui

---
+++ app/src/main/java/com/example/todolist12102022/AppConstant.java
@@ -0,0 +1,9 @@
+package com.example.todolist12102022;
+
+/**
+ * Created by pphat on 12/5/2022.
+ */
+public class AppConstant {
+    public static final int REQUEST_CODE_CAMERA = 1;
+    public static final int REQUEST_CODE_GALLERY = 2;
+}
--- app/src/main/java/com/example/todolist12102022/MainActivity.java
+++ app/src/main/java/com/example/todolist12102022/MainActivity.java
@@ -1,110 +1,20 @@
 package com.example.todolist12102022;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCamera, btnGallery;
-    ImageView img;
-    final int REQUEST_CODE_CAMERA = 1;
-    final int REQUEST_CODE_GALLERY = 2;
+    FloatingActionButton fAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fAB = findViewById(R.id.floating_button_add);
 
-        btnCamera = findViewById(R.id.buttonCamera);
-        btnGallery = findViewById(R.id.buttonGallery);
-        img = findViewById(R.id.imageView);
-
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Check permission granted
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    // Request permission
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
-                } else {
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent, REQUEST_CODE_CAMERA);
-                }
-            }
-        });
-
-        btnGallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Check permission granted
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    // Request permission
-                    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_GALLERY);
-                } else {
-                    Intent intent = new Intent(Intent.ACTION_PICK);
-                    intent.setType("image/*");
-                    startActivityForResult(intent, REQUEST_CODE_GALLERY);
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        int result = grantResults[0];
-        if (result != PackageManager.PERMISSION_GRANTED) return;
-        Intent intent;
-        switch (requestCode) {
-            case REQUEST_CODE_CAMERA:
-                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, REQUEST_CODE_CAMERA);
-                break;
-            default:
-                intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, REQUEST_CODE_GALLERY);
-                break;
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode != RESULT_OK && data == null) return;
-        switch (requestCode) {
-            case REQUEST_CODE_CAMERA:
-                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-                img.setImageBitmap(bitmap);
-                break;
-            default:
-                try {
-                    Uri imageUri = data.getData();
-                    InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                    Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                    img.setImageBitmap(selectedImage);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                break;
-        }
-
-        super.onActivityResult(requestCode, resultCode, data);
     }
 }
