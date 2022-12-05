369c661c5e631c987ac973d843e79f9ed2df68f9
Phat <phatdroid94@gmail.com>
2022-12-05 19:25:48 +0700

    event menu

--- app/src/main/java/com/example/todolist12102022/MainActivity.java
+++ app/src/main/java/com/example/todolist12102022/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.todolist12102022;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         fAB = findViewById(R.id.floating_button_add);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_sort_time:
+                Toast.makeText(this, "Click sort", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
