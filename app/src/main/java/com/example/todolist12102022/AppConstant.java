e7fdc4ad10f30a60c9988e43e8b362404a84088b
Phat <phatdroid94@gmail.com>
2022-12-09 19:51:44 +0700

    update

--- app/src/main/java/com/example/todolist12102022/AppConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.todolist12102022;
-
-/**
- * Created by pphat on 12/5/2022.
- */
-public class AppConstant {
-    public static final int REQUEST_CODE_CAMERA = 1;
-    public static final int REQUEST_CODE_GALLERY = 2;
-}
